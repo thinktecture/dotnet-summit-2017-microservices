a825217112aaae0a1f95fd2a9964687c09bf33f1
Christian Weyer <christian.weyer@thinktecture.com>
2017-05-04 11:16:32 +0200

    Prep for MyOrdersAppService

--- MyOrdersAppService/MyOrdersAppService/Program.cs
+++ MyOrdersAppService/MyOrdersAppService/Program.cs
@@ -7,22 +7,7 @@ namespace MyOrdersAppService
     {
         static void Main(string[] args)
         {
-            HostFactory.Run(x =>
-            {
-                x.Service<MyOrdersAppServiceHost>(s =>
-                {
-                    s.ConstructUsing(name => new MyOrdersAppServiceHost());
-                    s.WhenStarted(tc => tc.Start());
-                    s.WhenStopped(tc => tc.Stop());
-                });
-                x.RunAsNetworkService();
-
-                x.SetDescription("MyOrders App Service");
-                x.SetDisplayName("MyOrders App Service");
-                x.SetServiceName("MyOrdersAppService");
-            });
-
-            Console.ReadLine();
+            
         }
     }
 }
