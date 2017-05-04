ee67a0eea8d32d1bece675e5787562af39d93c8e
Christian Weyer <christian.weyer@thinktecture.com>
2017-05-04 16:11:56 +0200

    Consul integration

--- OrdersService/OrdersService/OrdersServiceHost.cs
+++ OrdersService/OrdersService/OrdersServiceHost.cs
@@ -4,20 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
+using Nanophone.Core;
+using Nanophone.RegistryHost.ConsulRegistry;
+using OrdersService.Discovery;
 
 namespace OrdersService
 {
     class OrdersServiceHost
     {
-        private string _baseUrl = "http://localhost:7777";
+        private Uri _baseUrl = new Uri("http://localhost:7777");
+        private Uri _healthUrl = new Uri("http://localhost:7777/api/health/ping");
 
-        private IDisposable _server { get; set; }
-
+        private static ServiceRegistry _serviceRegistry;
+        private static RegistryInformation _registryInformation;
+        private static IDisposable _server;
+        
         public void Start()
         {
-            _server = WebApp.Start<Startup>(_baseUrl);
+            var registryHost = new ConsulRegistryHost();
+            _serviceRegistry = new ServiceRegistry(registryHost);
 
-            Console.WriteLine("Order Service running...");
+            Task.Run(async () =>
+            {
+                _registryInformation = await _serviceRegistry.AddTenantAsync(
+                    new CustomWebApiRegistryTenant(_baseUrl), "orders-dns", "1.0.0", _healthUrl);
+
+                _server = WebApp.Start<Startup>(_baseUrl.ToString());
+                Console.WriteLine("Orders Service running - listening at {0} ...", _baseUrl);
+            }).GetAwaiter().GetResult();
+
         }
 
         public void Stop()
