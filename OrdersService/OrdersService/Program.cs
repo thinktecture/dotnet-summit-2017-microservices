5bc1bfd99923328dfdc08d0732814d53d31a2c25
Christian Weyer <christian.weyer@thinktecture.com>
2017-05-04 11:26:09 +0200

    Prep for OrdersService

--- OrdersService/OrdersService/Program.cs
+++ OrdersService/OrdersService/Program.cs
@@ -1,26 +1,10 @@
-﻿using Topshelf;
-
-namespace OrdersService
+﻿namespace OrdersService
 {
     public class Program
     {
         public static void Main()
         {
-            HostFactory.Run(x =>
-            {
-                x.Service<OrdersServiceHost>(s =>
-                {
-                    s.ConstructUsing(name => new OrdersServiceHost());
-                    s.WhenStarted(tc => tc.Start());
-                    s.WhenStopped(tc => tc.Stop());
-                });
-
-                x.RunAsNetworkService();
-
-                x.SetDisplayName("Orders Service");
-                x.SetDescription("Orders Service");
-                x.SetServiceName("OrdersService");
-            });
+           
         }
     }
 }
